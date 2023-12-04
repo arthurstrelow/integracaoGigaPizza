dbdbaa2972e8cc35d2b536231303c4597358a512
Arthur Strelow <arthurpbliima@gmail.com>
2023-12-04 16:16:34 -0300

    novas rotas; novas melhorias e refatoracao"

--- src/rotas/itemCompradoRotas.js
+++ src/rotas/itemCompradoRotas.js
@@ -2,9 +2,24 @@ import express from 'express'
 import * as itemCompradoControlador from '../controladores/itemCompradoControlador.js'
 const rotas = express.Router()
 
+rotas
+    .route('/listar/itenscomprados')
+    .get(itemCompradoControlador.obterItensComprados)
 
-/*rotas
-    .route('/[SUA ROTA]')
-    .get(itemCompradoControlador.[SUAS FUNÇÕES CRIADAS NO "itemCompradoControlador"])*/
+rotas
+    .route('/listar/itemcomprado/:id')
+    .get(itemCompradoControlador.obterItemComprado)
+
+rotas
+    .route('/ativar/compra')
+    .post(itemCompradoControlador.ativarCompra)
+
+rotas
+    .route('/inativar/compra')
+    .post(itemCompradoControlador.inativarCompra)
+
+rotas
+    .route('/cadastrar/itemcomprado')
+    .post(itemCompradoControlador.criaritemcomprado)
 
 export default rotas
